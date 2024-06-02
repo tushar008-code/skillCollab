3c8ee0112e6eb3ca0f945ae5c185411d6f8c96b0
tazi00 <tz0540245@gmail.com>
2024-06-01 22:24:20 -0700

    header changes

--- src/components/Header/components/OptionsBox/OptionsBox.tsx
+++ src/components/Header/components/OptionsBox/OptionsBox.tsx
@@ -1,29 +1,25 @@
 /** @format */
 
-import notifier from './assets/Group.png';
-import guestImg from './assets/guest.png';
+import notifier from "./assets/Group.png";
+import guestImg from "./assets/guest.png";
 
 function OptionsBox({ role }: { role: string }) {
-	return (
-		<div className='options order-3 flex-1 mobile:order-2 mobile:basis-1/2 '>
-			<ul className='flex items-center justify-end '>
-				<li className=' h-10 flex items-center justify-center w-10 rounded-full bg-white mobile:mx-2 '>
-					<img
-						className='h-5 w-5 object-cover'
-						src={notifier}
-						alt=''
-					/>
-				</li>
-				<li className='mx-5 mobile:mx-2'>
-					<img
-						className='h-9 w-9 object-cover mobile:h-8 mobile:w-8 rounded-full border-black border-solid border-1px '
-						src={role === 'guest' ? guestImg : ''}
-						alt=''
-					/>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div className="options order-3 flex-1 mobile:order-2 mobile:basis-1/2 ">
+      <ul className="flex items-center justify-end ">
+        <li className=" h-10 flex items-center justify-center w-10 rounded-full bg-white mobile:mx-0 ">
+          <img className="h-5 w-5 object-cover" src={notifier} alt="" />
+        </li>
+        <li className="mx-5 mobile:mx-2">
+          <img
+            className="h-9 w-9 object-cover mobile:h-8 mobile:w-8 rounded-full border-black border-solid border-1px "
+            src={role === "guest" ? guestImg : ""}
+            alt=""
+          />
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default OptionsBox;
